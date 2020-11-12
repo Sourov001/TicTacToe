2c480e7320e7a5c0ad5d5b577c5b8a72dd6539f7
Sourov Podder <sourov.podder@northsouth.edu>
2020-11-12 08:37:55 -0800

    Author Name Change

--- version_2/DumbAI.java
+++ version_2/DumbAI.java
@@ -1,7 +1,7 @@
 /**
  * Realization of AI interface using simplistic random placement strategy.
  *
- * @author INSERT YOUR NAME(S) HERE
+ * @author Sourov Podder Surzo
  */
 
 import java.util.Random;
--- version_2/SmartAI.java
+++ version_2/SmartAI.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Sourov Podder Surzo
  */
 
 import java.lang.UnsupportedOperationException;
